f9fb51b597ee7fc23583b338f1815639ab1dd52f
Stefano Mazzucco <stefano@curso.re>
2017-11-12 16:50:35 +0000

    Expose the Connman Technology API
    Connman Technologies are available in the "technologies" table of the
    Manager. Similarly to the services, they are updated in real time thanks to
    signals.
    
    The code have been refactored to be more general.

--- src/connman_dbus/init.lua
+++ src/connman_dbus/init.lua
@@ -18,7 +18,10 @@
   Get information about your network devices using
   [Connman](https://01.org/connman) and DBus.
 
-  Requiring this module will return the Connman Manager singleton.
+  Requiring this module will return the Connman @{Manager} singleton.
+
+  Proxy obejcts are updated in real time, provided that the code runs in a GLib
+  main loop (such as the Awesome Window Manager).
 
   **NOTE**: connman objects do **not** implement
   `org.freedesktop.DBus.Properties.PropertiesChanged`.
@@ -36,111 +39,38 @@ local function _update_property(self, prop_name, prop_value)
   self[prop_name] = prop_value
 end
 
---[[--
-  The Connman Manager singleton. This table is returned when the module is loaded with `require`.
-
-  You **must** use `Manager:SetProperty` to set the writeable properties (you
-  will need to wrap the value of the properti in an `lgi.GLib.Variant`), then
-  update them with `Manager:update_properties`.
-
-  Manager's properties:
-
-  - State: string. One of "offline", "idle", "ready" or "online".
-  - OfflineMode: boolean
-  - SessionMode: boolean (deprecated, always false)
-
-  For more information, see the [connman manager API
-  documentation](https://git.kernel.org/pub/scm/network/connman/connman.git/tree/doc/manager-api.txt)
-
-  @table Manager
-]]
-local Manager = proxy.Proxy:new(
-  {
-    bus = proxy.Bus.SYSTEM,
-    interface = "net.connman.Manager",
-    name = "net.connman",
-    path = "/"
-  }
-)
-
---[[--
-
-  Available Services.
-
-  Table containing Service objects. They can be accessed either using a numeric
-  index (e.g. `Manager.services[1]`) or using their object path
-  (e.g. `Manager.services["/net/connman/service/wifi_123_managed_psk"]`). The
-  lower its numeric index, the higher the priority the service has.
-
-  For more information, see the [connman Service API
-  documentation](https://git.kernel.org/pub/scm/network/connman/connman.git/tree/doc/service-api.txt).
-
-  Manager and  Services proxy obejcts are  updated in real time  (provided that
-  the code runs in a GLib main loop, such as the Awesome Window Manager).
-
-]]
-Manager.services = {}
-
-setmetatable(
-  Manager.services,
-  {
-    __index = function (t, k)
-      if type(k) == "number" then
-        for _, v in pairs(t) do
-          if v.n == k then
-            return v
-          end
-        end
-        return rawget(t, k)
-      end
-    end,
-    __newindex = function (t, k, v)
-      if type(k) == "number" then
-        for k1, v1 in pairs(t) do
-          if v1.n == k then
-            v.n = k
-            t[k1] = v
-            return
-          end
-        end
-      end
-      rawset(t, k, v)
-    end
-  }
-)
-
-local function _get_service(path)
-  local service = proxy.Proxy:new(
+local function _get(what, path)
+  local obj = proxy.Proxy:new(
     {
       bus = proxy.Bus.SYSTEM,
-      interface = "net.connman.Service",
+      interface = "net.connman." .. what,
       name = "net.connman",
       path = path
     }
   )
-  if service then
-    service:connect_signal(_update_property, "PropertyChanged")
+  if obj then
+    obj:connect_signal(_update_property, "PropertyChanged")
   end
-  return service
+  return obj
 end
 
---- Refresh the @{Manager.services} table.
-function Manager:refresh_services()
-  local pair_list = self:GetServices()
+local function _refresh(self, what, what_one)
+  local field = what:lower()
+  what_one = what_one or what:sub(1, -2)
+  local pair_list = self["Get" .. what](self)
 
   for idx, pair in ipairs(pair_list) do
     local path = pair[1]
-    local service = self.services[path] or _get_service(path)
-    if service then
-      service.n = idx
+    local obj = self[field][path] or _get(what_one, path)
+    if obj then
+      obj.n = idx
       local properties = pair[2]
       for k, v in pairs(properties) do
-        service[k] = v
+        obj[k] = v
       end
     end
-    self.services[path] = service
+    self[field][path] = obj
   end
-
 end
 
 local function _update_services(self, changed, removed)
@@ -163,16 +93,124 @@ local function _update_services(self, changed, removed)
   end
 end
 
-if Manager then
+local function _add_technology(self, path, properties)
+  local new_tech = _get("Technology", path)
+  for k, v in pairs(properties) do
+    new_tech[k] = v
+  end
+
+  local old_tech = self.technologies[path]
+  if old_tech then
+    new_tech.n = old_tech.n
+  else
+    new_tech.n = #self.technologies + 1
+  end
+
+  self.technologies[path] = new_tech
+end
+
+local function _remove_technology(self, path)
+  self.technologies[path] = nil
+end
+
+--- Metatable that delegates searching by index.
+local _delegate_index_mt = {
+  __index = function (t, k)
+    if type(k) == "number" then
+      for _, v in pairs(t) do
+        if v.n == k then
+          return v
+        end
+      end
+      return rawget(t, k)
+    end
+  end,
+  __newindex = function (t, k, v)
+    if type(k) == "number" then
+      for k1, v1 in pairs(t) do
+        if v1.n == k then
+          v.n = k
+          t[k1] = v
+          return
+        end
+      end
+    end
+    rawset(t, k, v)
+  end
+}
+
+--[[--
+  The Connman Manager singleton. This table is returned when the module is loaded with `require`.
+
+  You **must** use `Manager:SetProperty` to set the writeable properties (you
+  will need to wrap the value of the properti in an `lgi.GLib.Variant`), then
+  update them with `Manager:update_properties`.
+
+  Manager's properties:
+
+  - State: string. One of "offline", "idle", "ready" or "online".
+  - OfflineMode: boolean
+  - SessionMode: boolean (deprecated, always false)
+
+  For more information, see the [connman Manager API
+  documentation](https://git.kernel.org/pub/scm/network/connman/connman.git/tree/doc/manager-api.txt)
+
+  @table Manager
+]]
+local Manager = _get("Manager", "/")
+
+local function _init()
   for k, v in pairs(Manager:GetProperties()) do
     Manager[k] = v
   end
-
   Manager:refresh_services()
-
-  Manager:connect_signal(_update_property, "PropertyChanged")
-
   Manager:connect_signal(_update_services, "ServicesChanged")
+  Manager:refresh_technologies()
+  Manager:connect_signal(_add_technology, "TechnologyAdded")
+  Manager:connect_signal(_remove_technology, "TechnologyRemoved")
 end
+
+--[[--
+
+  Available Services.
+
+  Table containing Service objects. They can be accessed either using a numeric
+  index (e.g. `Manager.services[1]`) or using their object path
+  (e.g. `Manager.services["/net/connman/service/wifi_123_managed_psk"]`). The
+  lower its numeric index, the higher the priority the service has.
+
+  For more information, see the [connman Service API
+  documentation](https://git.kernel.org/pub/scm/network/connman/connman.git/tree/doc/service-api.txt).
+
+]]
+Manager.services = {}
+setmetatable(Manager.services, _delegate_index_mt)
+
+--[[--
+
+  Available Technologies.
+
+  Table containing Technology objects. They can be accessed either using a
+  numeric index (e.g. `Manager.technologies[1]`) or using their object path
+  (e.g. `Manager.technologies["/net/connman/technology/wifi"]`).
+
+  For more information, see the [connman Technology API
+  documentation](https://git.kernel.org/pub/scm/network/connman/connman.git/tree/doc/technology-api.txt).
+
+]]
+Manager.technologies = {}
+setmetatable(Manager.technologies, _delegate_index_mt)
+
+--- Refresh the @{Manager.services} table.
+function Manager:refresh_services()
+  _refresh(self, "Services")
+end
+
+--- Refresh the @{Manager.technologies} table.
+function Manager:refresh_technologies()
+  _refresh(self, "Technologies", "Technology")
+end
+
+_init()
 
 return Manager
